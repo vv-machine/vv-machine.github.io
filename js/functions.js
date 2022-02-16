b1a81f90f444d56653b4d6f30609a97a354d8f54
vv-machine <hello@vv-machine.com>
2022-02-16 10:02:38 +0100

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  var overlay = $('.nav'),
+      container = $('body');
+
+  if ($(window).width() < 1200) {
+   addHamburger();
+  }
+
+  $('.lb__button').click(function(e) {
+    e.preventDefault();
+    window.location.hash="";
+  });
+
+  var resizeId;
+
+  $(window).resize(function() {
+        clearTimeout(resizeId);
+        if ($(window).width() < 1200) {
+          resizeId = setTimeout(addHamburger, 500);
+        }
+        else if(($('.button--hamburger').length) && ($(window).width() > 1200) ) {
+            $('.button--hamburger').remove();
+            $(container).removeClass("noscroll");
+            $(overlay).removeClass("nav--active");
+        }
+    });
+
+    function addHamburger(){
+      if( !$('.button--hamburger').length )  {
+        $(overlay).after('<button class="button button--hamburger"><span></span></button>');
+        $(overlay).addClass("nav--hide");
+    }
+
+
+    $('.button--hamburger').click(function(e) {
+           e.preventDefault(e);
+           toggleActive();
+    });
+
+
+    $('.nav__link--info').click(function(e) {
+      setTimeout(toggleActive,500);
+
+    });
+
+
+    function toggleActive() {
+      $(overlay).toggleClass("nav--active");
+      $(container).toggleClass("noscroll");
+      $('.button--hamburger').toggleClass("close");
+    }
+
+  }
+
+    $('.mapboxgl-canvas').on('idle',function(){
+    $('.mapboxgl-canvas').resize()
+  })
+
+});
